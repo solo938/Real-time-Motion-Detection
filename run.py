8d06f18f514a97ddc72faf1b00348366f2b64566
Sahariar Hasan <68711966+solo938@users.noreply.github.com>
2026-03-28 17:18:10 +0530

    Update app initialization and run configuration

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app
 
-app = create_app('development')  
+app = create_app()  
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
